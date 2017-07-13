f135ccd383560c3853bf18c76763add32e8d5a38
hhimanshu <harit.himanshu@yahoo.in>
2017-07-14 06:14:38 +1200

    added bundling

---
+++ webpack.config.dev.js
@@ -0,0 +1,25 @@
+import path from 'path';
+
+// https://gist.github.com/coryhouse/d611e83e432f3ae65cc46ebb9b599930
+
+export default {
+  debug: true,
+  devtool: 'inline-source-map',
+  noInfo: false,
+  entry: [
+    path.resolve(__dirname, 'src/index')
+  ],
+  target: 'web',
+  output: {
+    path: path.resolve(__dirname, 'src'),
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  plugins: [],
+  module: {
+    loaders: [
+      {test: /\.js$/, exclude: /node_modules/, loaders: ['babel']},
+      {test: /\.css$/, loaders: ['style','css']}
+    ]
+  }
+}
