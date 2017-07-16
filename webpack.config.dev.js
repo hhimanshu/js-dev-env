b3ad2058e2becbae256c67b52b42c0a856438399
hhimanshu <harit.himanshu@yahoo.in>
2017-07-17 08:55:26 +1200

    added Production build

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,4 +1,5 @@
 import path from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
 
 // https://gist.github.com/coryhouse/d611e83e432f3ae65cc46ebb9b599930
 
@@ -15,7 +16,13 @@ export default {
     publicPath: '/',
     filename: 'bundle.js'
   },
-  plugins: [],
+  plugins: [
+    // Create HTML file that includes reference to bundled JS.
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+      inject: true
+    })
+  ],
   module: {
     loaders: [
       {test: /\.js$/, exclude: /node_modules/, loaders: ['babel']},
